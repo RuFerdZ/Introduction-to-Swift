5573f5e9cf98927ca877fca04507e356a018a20f
RuFerdZ <ge.rusiru@gmail.com>
2021-07-31 01:19:15 +0530

    added inital commit

---
+++ codes/01-MyPlayground.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+// variables
+var str = "Hello World"
+var x = 4
+var y = 1
+
+// constants
+let z = 10
+
+class Person {
+    static let age = 12
+    var lastname = "Smith"
+}
+
+var john = Person()
+john.lastname
+
+var Tim = Person()
+var Ben = Person()
+var Ann = Person()
+
+let person = Person.age
+
