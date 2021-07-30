b2067af78c75e49f019dc13866e489ad01d24778
RuFerdZ <ge.rusiru@gmail.com>
2021-07-31 01:35:37 +0530

    added data types

---
+++ codes/02-Types.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+// Types
+var x = 3                // Integer
+var name = "Rusiru"      // String
+
+
+// check the datatype of a variable
+type(of: x)     //  Int.Type
+type(of: name)  //  String.Type
+
+
+// explicit declarations
+var lastname: String = "last"
+var age: Int = 10
+
+// constant
+let PI: Double = 3.142;
+
+var height: Float = 200.0293123012321
+
+var isComplete: Bool = false
+isComplete = true
+
+class Person {
+
+}
+
+var john: Person = Person()
+// or
+var peter = Person()
+
+var button1: UIButton = UIButton()
+// or 
+var button2 = UIButton()
+
+
